6209d84a190e858a1d27109dff0754d71aaf7b07
Matej <mrennert91@gmail.com>
2018-03-14 22:47:52 +0100

    UEX-0001 ADD Enable data binding

--- app/src/main/java/androidappteam/com/universityexchange/SplashActivity.java
+++ app/src/main/java/androidappteam/com/universityexchange/SplashActivity.java
@@ -1,13 +1,24 @@
 package androidappteam.com.universityexchange;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidappteam.com.universityexchange.databinding.ActivitySplashBinding;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding splashBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        prepareNecessaryData();
+    }
+
+    private void prepareNecessaryData() {
+        splashBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
     }
 }
